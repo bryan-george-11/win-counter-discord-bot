b0f0e7d2329405f728c414d6c3df734471e9a8b9
bryan-george-11 <51124547+bryan-george-11@users.noreply.github.com>
2023-04-29 18:36:02 -0400

    feat(warzone-win-bot): set up help command
    this command will loop through all prefixes and pull commands and display the name and description
    
    \

--- commands/prefix/General/help.js
+++ commands/prefix/General/help.js
@@ -1,5 +1,4 @@
 const { EmbedBuilder } = require("discord.js");
-const fs = require('fs');
 
 module.exports = {
   config: {
@@ -8,28 +7,31 @@ module.exports = {
   },
   permissions: ['SendMessages'],
   owner: false,
-  run: async (client, message, args, prefix) => {
-    const commands = client.prefix_commands.map(command => `${prefix}${command.config.name}`);
+  run: async (client, message, args, prefix, config, database, db) => {
+    try {
+      const commandName = client.prefix_commands.map(command => `${prefix}${command.config.name}`);
+      const commandDescription = client.prefix_commands.map(command => `${command.config.description}`);
 
-    return message.reply(
-      {
-        embeds: [
-          new EmbedBuilder()
-            .setTitle('Warzone Wins Discord Bot Help')
-            .setColor('bf1919')
-            .setDescription('Here are the available commands for this bot:')
-            .addFields(
-              { name: `${prefix}win`, value: 'Add a win for one or more users. If no users are mentioned, adds a win for the message author.' },
-              { name: `${prefix}removewin`, value: 'Remove a win for a specific user.' },
-              { name: `${prefix}leaderboard`, value: 'Show the top 5 users with the most wins.' },
-              { name: `${prefix}resetAllWins`, value: 'Reset all win totals to 0.' },
-            )
-            .setTimestamp()
-            .setAuthor({ name: 'Bot Helper', iconURL: 'https://media.istockphoto.com/id/1128516744/vector/robot-icon-bot-sign-design-chatbot-symbol-concept-voice-support-service-bot-online-support.jpg?s=612x612&w=0&k=20&c=Z8MetVg7f2qkrg6pEBl2LOvqGcZp8z46YE-QywHIP8A=' })
-            .setThumbnail('https://media.istockphoto.com/id/1128516744/vector/robot-icon-bot-sign-design-chatbot-symbol-concept-voice-support-service-bot-online-support.jpg?s=612x612&w=0&k=20&c=Z8MetVg7f2qkrg6pEBl2LOvqGcZp8z46YE-QywHIP8A=')
-          ]
+      const commandFields = [];
+      for (const [index, name] of commandName.entries()) {
+        const description = commandDescription[index];
+        commandFields.push({ name: name, value: description });
       }
-    );
+
+      const helpCommands = new EmbedBuilder()
+        .setTitle('Warzone Wins Discord Bot Help')
+        .setColor('bf1919')
+        .setDescription('Here are the available commands for this bot:')
+        .addFields(...commandFields)
+        .setTimestamp()
+        .setAuthor({ name: 'Bot Helper', iconURL: 'https://media.istockphoto.com/id/1128516744/vector/robot-icon-bot-sign-design-chatbot-symbol-concept-voice-support-service-bot-online-support.jpg?s=612x612&w=0&k=20&c=Z8MetVg7f2qkrg6pEBl2LOvqGcZp8z46YE-QywHIP8A=' })
+
+      message.channel.send({ embeds: [helpCommands] });
+
+    } catch (err) {
+      console.log(err);
+      message.reply('Error occurred while attempting to get help menu');
+    }
 
   },
 };
