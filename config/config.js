9c2e0d7f532b34681a8a39bc052f922e9782a6ef
bryan-george-11 <51124547+bryan-george-11@users.noreply.github.com>
2023-06-04 14:24:33 -0400

    update bot ID

--- config/config.js
+++ config/config.js
@@ -5,9 +5,10 @@ module.exports = {
   Users: {
     OWNERS: ["849413565487382578", "554029264492691476"] // THE BOT OWNERS ID.
   },
+  // Need to make dynamic for testing prod bot id 1078162475779899502 dev bot id 1083854818008907827
 
   Client: {
-    ID: "1083854818008907827" // YOUR BOT ID.
+    ID: "1078162475779899502" // YOUR BOT ID.
   }
 
 }
