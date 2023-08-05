67816ffedb1e0e72bc613a9ac07605503c795da2
Bryan George <bcgeorge11@gmail.com>
2023-08-05 17:21:54 -0400

    feat: update wins command to update season wins and career wins

--- commands/prefix/Warzone/seasonWins.js
+++ commands/prefix/Warzone/seasonWins.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
     config: {
         name: "win",
-        description: "Add wins to the leaderboard",
+        description: "Add wins for to the leaderboard",
     },
     permissions: ['SendMessages'],
     owner: false,
@@ -17,20 +17,24 @@ module.exports = {
                 userIds = [message.author.id];
             }
             const collection = await database.collection(process.env.DB_COLLECTION_NAME);
-            const updates = userIds.map(userId => collection.updateOne({ _id: userId }, { $inc: { wins: 1 } }, { upsert: true }));
-            await Promise.all(updates);
+            const updateSeason = userIds.map(userId => collection.updateOne({ _id: userId }, { $inc: { seasonWins: 1 } }, { upsert: true }));
+            const updateCareer = userIds.map(userId => collection.updateOne({ _id: userId }, { $inc: { wins: 1 } }, { upsert: true }));
+            await Promise.all(updateSeason, updateCareer);
 
             // Get the updated win totals for each user from the database
             const users = await collection.find({ _id: { $in: userIds } }).toArray();
-            const winTotals = users.map((user, index) => `${index + 1} - ${message.guild.members.cache.get(user._id).displayName}: ${user.wins || 0}`).join('\n');
+            const seasonWinTotals = users.map((user, index) => `${index + 1} - ${message.guild.members.cache.get(user._id).displayName}: ${user.seasonWins || 0}`).join('\n');
+
+            let currentSeason = 'Season 5'
 
             // Reply with the updated win totals for each user
             const winTrackerEmbed = new EmbedBuilder()
                 .setColor('F1C40F')
-                .setTitle('COD Warzone Season 4 Warzone Victory')
-                .setAuthor({ name: 'Warzone Victory', iconURL: 'https://as2.ftcdn.net/v2/jpg/01/39/31/79/1000_F_139317922_FAWtQJMMVOVvDeM2OVg0ofiwIvBUrrux.jpg' })
-                .setDescription(`Win Totals Below Have now been updated:\n\n ${winTotals}`)
-                .setThumbnail(client.user.displayAvatarURL(({ format: 'png', size: 4096 })))
+                .setTitle(`COD Warzone ${currentSeason} Warzone Victory`)
+                .setAuthor({ name: `${currentSeason} Win`, iconURL: 'https://as2.ftcdn.net/v2/jpg/01/39/31/79/1000_F_139317922_FAWtQJMMVOVvDeM2OVg0ofiwIvBUrrux.jpg' })
+                .setDescription(`Win Totals Below Have now been updated:\n\n ${seasonWinTotals}`)
+                .setThumbnail('https://wzhub.gg/images/ranked-header-2.webp')
+                //.setFooter({ text: 'Ranked mode', iconURL: 'https://wzhub.gg/images/ranked-header-2.webp' })
                 .setTimestamp()
 
             message.channel.send({ embeds: [winTrackerEmbed] });
