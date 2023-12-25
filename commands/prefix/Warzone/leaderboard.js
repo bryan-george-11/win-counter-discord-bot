564b2ea5216191beb2d7774b4dbaf9f12b084b14
Bryan George <bcgeorge11@gmail.com>
2023-12-25 14:05:05 -0500

    feat: update to warzone season 3 - update sort

--- commands/prefix/Warzone/leaderboard.js
+++ commands/prefix/Warzone/leaderboard.js
@@ -10,27 +10,30 @@ module.exports = {
   run: async (client, message, args, prefix, config, database, db) => {
     try {
       const collection = await database.collection(process.env.DB_COLLECTION_NAME);
-      const top5 = await collection.find().sort({ wins: -1 }).limit(5).toArray();
-      console.log("🚀 ~ file: leaderboard.js:20 ~ run: ~ top5:", top5)
-      const leaderboard = top5.map((user, index) => `${index + 1} - <@${user._id}> - ${user.wins} wins`).join('\n');
-      const firstPerson = top5[0]._id
+      const seasonTop5 = await collection.find().sort({ seasonWins: -1 }).limit(5).toArray();
+      const careerTop5 = await collection.find().sort({ wins: -1 }).limit(5).toArray();
+      const leaderboard = careerTop5.map((user, index) => `${index + 1} - <@${user._id}> - ${user.wins} wins`).join('\n');
+      const firstPerson = careerTop5[0]._id
       const theLeader = client.users.cache.get(firstPerson);
 
-      const seasonLeaderboard = top5.map((user, index) => `${index + 1} - <@${user._id}> - ${!user.seasonWins ? `_Do you think you'll ever manage to get a win?_` : user.seasonWins + `${user.seasonWins === 1 ? ` win` : ` wins`}`}`).join('\n');
-      const seasonFirstPerson = top5[0]._id
+      const seasonLeaderboard = seasonTop5.map((user, index) => `${index + 1} - <@${user._id}> - ${!user.seasonWins ? `_Do you think you'll ever manage to get a win?_` : user.seasonWins + `${user.seasonWins === 1 ? ` win` : ` wins`}`}`).join('\n');
+      const seasonFirstPerson = seasonTop5[0]._id
+      console.log("🚀 ~ file: leaderboard.js:21 ~ run: ~ seasonLeaderboard:", seasonLeaderboard)
       const theSeasonLeader = client.users.cache.get(seasonFirstPerson);
 
-      let currentSeason = 'Season 5'
+      
+
+      let currentSeason = 'Warzone 3 Season 1'
       const rand = Math.random().toString(36).slice(2);
 
       // Reply with the top 5 leaderboard
       const leaderboardEmbed = new EmbedBuilder()
         .setColor('F1C40F')
-        .setTitle(`COD Warzone ${currentSeason} Wins Leaderboard`)
+        .setTitle(`COD ${currentSeason} Wins Leaderboard`)
         .setAuthor({ name: 'Leaderboard', iconURL: 'https://as2.ftcdn.net/v2/jpg/01/39/31/79/1000_F_139317922_FAWtQJMMVOVvDeM2OVg0ofiwIvBUrrux.jpg' })
         .setDescription(`**_${currentSeason} Win Totals:_**\n${seasonLeaderboard} \n\n **_Career Win Totals:_**\n${leaderboard} \n\n`)
         .setThumbnail(theSeasonLeader.avatarURL({ format: 'png', size: 4096 }))
-        .setImage(`https://www.dexerto.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/08/02/warzone-2-s5-patch-notes.jpg?${rand}`)
+        .setImage(`https://www.charlieintel.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.charlieintel.com/wp-content/uploads/2023/12/06/season-1-modern-warfare-3-update.jpg?${rand}`)
         .setFooter({ text: 'Victory', iconURL: `https://static.wikia.nocookie.net/callofduty/images/4/47/RankedPlay_Logo_MWII.png/revision/latest/scale-to-width-down/250?cb=20230227180441?${rand}` })
         .setTimestamp()
 
